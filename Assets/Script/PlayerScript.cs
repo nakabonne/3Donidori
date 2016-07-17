1db23aaf52375eeeb54b14b5242d367e029e56be
ryonakao <rodriguez.nak@gmail.com>
2016-07-18 01:45:35 +0900

    緑のマテリアルとスマホアセットインポートした

--- Assets/Script/PlayerScript.cs
+++ Assets/Script/PlayerScript.cs
@@ -2,7 +2,8 @@
 using System.Collections;
 using UnityStandardAssets.CrossPlatformInput;
 
-public class PlayerScript : MonoBehaviour {
+public class PlayerScript : MonoBehaviour 
+{
 
 	GameManager gamemanager;
 	public GameObject GBtext;
@@ -27,7 +28,7 @@ public class PlayerScript : MonoBehaviour {
 		//タッチすると方向転換
 		if (Input.GetMouseButtonDown (0)) 
 		{
-			if (!migi)
+			/*if (!migi)
 			{
 				transform.Rotate (new Vector3 (0, -90, 0));
 				Debug.Log ("右");
@@ -41,68 +42,33 @@ public class PlayerScript : MonoBehaviour {
 				Invoke ("False",0.00000001f);
 				//migi = false;
 
-			}
+			}*/
+
+
+
+				if(migi)
+			    {
+				transform.Rotate(new Vector3(0,90,0));
+				}
+			    else
+			    {
+				transform.Rotate(new Vector3(0,-90,0));
+				}
+				migi = !migi;
 
 		}
 
-
-		/*if (migi == true) 
-		{
-			transform.position += new Vector3 (1, 0, 1) * speed * Time.deltaTime;
-
-		}
-
-		if (migi == false) 
-		{
-			transform.position += new Vector3 (-1, 0, 1) * speed * Time.deltaTime;
-
-		}*/
+	}
 
 
-		//Idou ();
-
-		/*StartCoroutine ("Houkoutenkan");
-		if (Input.GetMouseButtonDown(0)) 
-		{
-			houkou = 1;
-		}
-
-
-		if (migi == false && Input.GetMouseButtonDown(0)) 
-			{
-				transform.position += new Vector3 (-1, 0, 1) * speed * Time.deltaTime;
-				
-			        migi = true;
-					Debug.Log ("下");
-	        }
-			Debug.Log (migi);
-
-
-			
-		if (migi == true && Input.GetMouseButtonDown(0)) 
-		    {
-			   transform.position += new Vector3 (1, 0, 1) * speed * Time.deltaTime;
-
-
-				migi = false;
-				Debug.Log ("上");
-
-
-		    }*/
-
+		
 
 
 			
 		
-	}
 
-	void True(){
-		migi = true;
-	}
 
-	void False(){
-		migi = false;
-	}
+
 
 	void OnTriggerEnter(Collider other)
 	{
@@ -113,52 +79,9 @@ public class PlayerScript : MonoBehaviour {
 			gamemanager.GameOver ();
 		}
 	}
-	//タッチで方向転換をする
-	/*IEnumerator Houkoutenkan()
-	{
-		transform.position += new Vector3 (1, 0, 1) * speed * Time.deltaTime;
-		yield return new WaitUntil(houkou == 0);
-
-
-	}
 
 
 
 
-	void Idou()
-	{
-
-		if (Input.GetMouseButtonDown (0)) 
-		{
-			int i = 0;
-			while (i < 1)
-			{
-				
-					transform.position += new Vector3 (1, 0, 1) * speed * Time.deltaTime;
-				
-				if (Input.GetMouseButtonDown (0)) 
-				{
-					i = 2;
-					//migi = false;
-
-				}
-			}
-
-			while (i > 1) 
-			{
-
-
-					transform.position += new Vector3 (-1, 0, 1) * speed * Time.deltaTime;
-
-				
-
-				if (Input.GetMouseButtonDown (0)) 
-				{
-					i = 0;
-					//migi = true;
-				}
-			}
-		}
-		//これだと毎フレームごとに上の移動が実行されるからタッチしてもすぐに戻っちゃうifをifで囲む（もし右いってたら左いくみたいな分岐）
-	}*/
+	
 }
